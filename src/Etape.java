ba2754921c3678d6f3523b9077bb1eb418b3951e
wafo vianey <wafodarius2@gmail.com>
2026-03-14 13:11:37 +0100

    deuxieme sauvegarde de gestion des trajets

---
+++ src/Etape.java
@@ -0,0 +1,125 @@
+
+
+import java.time.LocalDateTime;
+
+
+public class Etape {
+
+
+    private Integer id;
+    private String nomEtape;
+    private String ville;
+    private String pays;
+    private LocalDateTime heureDepartEtape;
+    private LocalDateTime heureArriveEtape;
+    private Integer dureeArret;
+    private Enum typeEtape;
+    private Integer ordre;
+    private String description;
+    private LocalDateTime dateCreation;
+
+
+    public Etape(Integer id, String nomEtape, String ville, String pays, LocalDateTime heureDepartEtape, LocalDateTime heureArriveEtape, Integer dureeArret, Enum typeEtape, Integer ordre, String description, LocalDateTime dateCreation) {
+        this.id = id;
+        this.nomEtape = nomEtape;
+        this.ville = ville;
+        this.pays = pays;
+        this.heureDepartEtape = heureDepartEtape;
+        this.heureArriveEtape = heureArriveEtape;
+        this.dureeArret = dureeArret;
+        this.typeEtape = typeEtape;
+        this.ordre = ordre;
+        this.description = description;
+        this.dateCreation = dateCreation;
+    }
+
+    public Etape() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNomEtape() {
+        return nomEtape;
+    }
+
+    public void setNomEtape(String nomEtape) {
+        this.nomEtape = nomEtape;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public String getPays() {
+        return pays;
+    }
+
+    public void setPays(String pays) {
+        this.pays = pays;
+    }
+
+    public LocalDateTime getHeureDepartEtape() {
+        return heureDepartEtape;
+    }
+
+    public void setHeureDepartEtape(LocalDateTime heureDepartEtape) {
+        this.heureDepartEtape = heureDepartEtape;
+    }
+
+    public LocalDateTime getHeureArriveEtape() {
+        return heureArriveEtape;
+    }
+
+    public void setHeureArriveEtape(LocalDateTime heureArriveEtape) {
+        this.heureArriveEtape = heureArriveEtape;
+    }
+
+    public Integer getDureeArret() {
+        return dureeArret;
+    }
+
+    public void setDureeArret(Integer dureeArret) {
+        this.dureeArret = dureeArret;
+    }
+
+    public Enum getTypeEtape() {
+        return typeEtape;
+    }
+
+    public void setTypeEtape(Enum typeEtape) {
+        this.typeEtape = typeEtape;
+    }
+
+    public Integer getOrdre() {
+        return ordre;
+    }
+
+    public void setOrdre(Integer ordre) {
+        this.ordre = ordre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getDateCreation() {
+        return dateCreation;
+    }
+
+    public void setDateCreation(LocalDateTime dateCreation) {
+        this.dateCreation = dateCreation;
+    }
+}
--- src/Trajet.java
+++ src/Trajet.java
@@ -1,51 +1,173 @@
-import java.util.Date;
+import java.time.LocalDateTime;
+
+
 
 public class Trajet {
-    String nomTrajet;
-    String numTrajet;
-    Integer durée;
-    Date dateDepart;
+   private Integer idTrajet;
+   private String villeDepart;
+   private String villeArrivee;
+   private String paysDepart;
+   private String paysArrivee;
+   private LocalDateTime dateHeureDepart;
+   private LocalDateTime dateHeureArrivee;
+   private Integer duree;
+   private Enum typeTransport;
+   private String numVol_bus;
+   private String nomCompagnie;
+   private Integer ordreTrajet;
+   private Enum statut;
+   private String description;
+   private LocalDateTime dateCreation;
+   private LocalDateTime dateModification;
 
 
-    public Trajet(String nomTrajet, String numTrajet) {
-        this.nomTrajet = nomTrajet;
-        this.numTrajet = numTrajet;
-        this.dateDepart = new Date();
-        this.durée = durée;
+    public Trajet(Integer idTrajet, String villeDepart, String villeArrivee, String paysDepart, String paysArrivee, LocalDateTime dateHeureDepart, LocalDateTime dateHeureArrivee, Integer duree, Enum typeTransport, String numVol_bus, String nomCompagnie, Integer ordreTrajet, Enum statut, String description, LocalDateTime dateCreation, LocalDateTime dateModification) {
+        this.idTrajet = idTrajet;
+        this.villeDepart = villeDepart;
+        this.villeArrivee = villeArrivee;
+        this.paysDepart = paysDepart;
+        this.paysArrivee = paysArrivee;
+        this.dateHeureDepart = dateHeureDepart;
+        this.dateHeureArrivee = dateHeureArrivee;
+        this.duree = duree;
+        this.typeTransport = typeTransport;
+        this.numVol_bus = numVol_bus;
+        this.nomCompagnie = nomCompagnie;
+        this.ordreTrajet = ordreTrajet;
+        this.statut = statut;
+        this.description = description;
+        this.dateCreation = dateCreation;
+        this.dateModification = dateModification;
     }
 
     public Trajet() {
     }
 
-    public String getNomTrajet() {
-        return nomTrajet;
+    public Integer getIdTrajet() {
+        return idTrajet;
     }
 
-    public void setNomTrajet(String nomTrajet) {
-        this.nomTrajet = nomTrajet;
+    public void setIdTrajet(Integer idTrajet) {
+        this.idTrajet = idTrajet;
     }
 
-    public String getNumTrajet() {
-        return numTrajet;
+    public String getVilleDepart() {
+        return villeDepart;
     }
 
-    public void setNumTrajet(String numTrajet) {
-        this.numTrajet = numTrajet;
+    public void setVilleDepart(String villeDepart) {
+        this.villeDepart = villeDepart;
     }
 
-    public Integer getDurée() {
-        return durée;
+    public String getVilleArrivee() {
+        return villeArrivee;
     }
 
-    public void setDurée(Integer durée) {
-        this.durée = durée;
+    public void setVilleArrivee(String villeArrivee) {
+        this.villeArrivee = villeArrivee;
     }
 
-    public Date getDateDepart() {
-        return dateDepart;
+    public String getPaysDepart() {
+        return paysDepart;
     }
 
-    public void setDateDepart(Date dateDepart) {
-        this.dateDepart = dateDepart;
+    public void setPaysDepart(String paysDepart) {
+        this.paysDepart = paysDepart;
+    }
+
+    public String getPaysArrivee() {
+        return paysArrivee;
+    }
+
+    public void setPaysArrivee(String paysArrivee) {
+        this.paysArrivee = paysArrivee;
+    }
+
+    public LocalDateTime getDateHeureDepart() {
+        return dateHeureDepart;
+    }
+
+    public void setDateHeureDepart(LocalDateTime dateHeureDepart) {
+        this.dateHeureDepart = dateHeureDepart;
+    }
+
+    public LocalDateTime getDateHeureArrivee() {
+        return dateHeureArrivee;
+    }
+
+    public void setDateHeureArrivee(LocalDateTime dateHeureArrivee) {
+        this.dateHeureArrivee = dateHeureArrivee;
+    }
+
+    public Integer getDuree() {
+        return duree;
+    }
+
+    public void setDuree(Integer duree) {
+        this.duree = duree;
+    }
+
+    public Enum getTypeTransport() {
+        return typeTransport;
+    }
+
+    public void setTypeTransport(Enum typeTransport) {
+        this.typeTransport = typeTransport;
+    }
+
+    public String getNumVol_bus() {
+        return numVol_bus;
+    }
+
+    public void setNumVol_bus(String numVol_bus) {
+        this.numVol_bus = numVol_bus;
+    }
+
+    public String getNomCompagnie() {
+        return nomCompagnie;
+    }
+
+    public void setNomCompagnie(String nomCompagnie) {
+        this.nomCompagnie = nomCompagnie;
+    }
+
+    public Integer getOrdreTrajet() {
+        return ordreTrajet;
+    }
+
+    public void setOrdreTrajet(Integer ordreTrajet) {
+        this.ordreTrajet = ordreTrajet;
+    }
+
+    public Enum getStatut() {
+        return statut;
+    }
+
+    public void setStatut(Enum statut) {
+        this.statut = statut;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getDateCreation() {
+        return dateCreation;
+    }
+
+    public void setDateCreation(LocalDateTime dateCreation) {
+        this.dateCreation = dateCreation;
+    }
+
+    public LocalDateTime getDateModification() {
+        return dateModification;
+    }
+
+    public void setDateModification(LocalDateTime dateModification) {
+        this.dateModification = dateModification;
     }
 }
